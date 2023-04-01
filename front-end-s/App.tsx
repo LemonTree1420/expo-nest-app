d8a30f6932d4a2a3ed09ffb3a2581713ab9d13b9
Ubuntu <ubuntu@ip-172-26-14-104.ap-northeast-2.compute.internal>
2023-04-01 06:11:07 +0000

    build try

--- front-end-s/App.tsx
+++ front-end-s/App.tsx
@@ -1,18 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { Provider as PaperProvider } from "react-native-paper";
-import React, { useEffect } from "react";
+import React from "react";
 import { RecoilRoot } from "recoil";
 import Navigation from "./navigation";
 import { useNetInfo, NetInfoState } from "@react-native-community/netinfo";
 import NoNetWork from "./src/Screens/noNetWork";
-import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
   const internetState: NetInfoState = useNetInfo();
-
-  useEffect(() => {
-    SplashScreen.hideAsync();
-  }, []);
 
   return (
     <RecoilRoot>
